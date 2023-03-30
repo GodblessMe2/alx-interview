8744b4a0c2a8e3584d121fdaa34bbe5f30515346
GodblessMe2 <oshindeinde@gmail.com>
2023-03-30 17:26:50 +0100

    correction

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python3
 
 def canUnlockAll(boxes):
-    unlocked = [0]
-    for box_id, box in enumerate(boxes):
-        if not box:
-            continue
-        for key in box:
-            if key < len(boxes) and key not in unlocked and key != box_id:
-                unlocked.append(key)
-    if len(unlocked) == len(boxes):
-        return True
-    else:
-        return False
+    '''
+    Determines if all the boxes can be opened or not.
+
+    Returns:
+        True if all boxes can be opened, else False.
+    '''
+    n = len(boxes)
+    unlocked = [False] * n
+    unlocked[0] = True
+    keys = set(boxes[0])
+
+    while keys:
+        box_to_open = keys.pop()
+        if not unlocked[box_to_open]:
+            unlocked[box_to_open] = True
+            keys.update(boxes[box_to_open])
+
+    return all(unlocked)
