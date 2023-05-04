5647334d0e65d19fd343f8f0a94dd21d8cf0bab7
GodblessMe2 <oshindeinde@gmail.com>
2023-05-04 13:10:28 +0100

    Worked on the starwars algorithms

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+
+const request = require('request');
+
+if (process.argv.length > 2) {
+  request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], (err, body) => {
+    if (err) throw err
+    const charactersURL = JSON.parse(body).characters;
+    const charactersByName = charactersURL.map(
+      url => new Promise((resolve, reject) => {
+        request(url, (promiseErr, __, charactersReqBody) => {
+          if(promiseErr) {
+            reject(promiseErr);
+          }
+          resolve(JSON.parse(charactersReqBody).name);
+       });
+      })
+    );
+    Promise.all(charactersByName)
+      .then(names => console.log(names.join('\n')))
+      .catch(err => console.log(err));
+  });
+}
