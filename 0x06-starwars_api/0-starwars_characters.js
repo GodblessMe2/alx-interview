2c16a514bb80b705cf3c22f3f587e547a29874e8
GodblessMe2 <oshindeinde@gmail.com>
2023-05-04 14:22:59 +0100

    Corrections

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
-
-
 const request = require('request');
 
 if (process.argv.length > 2) {
   request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], (err, _, body) => {
-    if (err) throw err
+    if (err) console.log(err)
     const charactersURL = JSON.parse(body).characters;
     const charactersByName = charactersURL.map(
       url => new Promise((resolve, reject) => {
